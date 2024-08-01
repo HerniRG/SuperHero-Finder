bad8b44a6cc02ab5f700a24619b84a7aa61f4c80
hernirg <hernanrg85@gmail.com>
2024-08-01 16:53:46 +0200

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by Hernán Rodríguez on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
